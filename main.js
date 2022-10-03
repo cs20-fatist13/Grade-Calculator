67c6550003742097794c5cff88347d0a3c7f6d89
b.anwar <109045924+fatist13@users.noreply.github.com>
2022-10-03 13:38:39 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 // JavaScript Code
 // Input
+document.getElementById("btn").addEventListener("click", sayButton);
+
+function sayButton() {
+    alert("Hello Button");
+}
+
+document.getElementById("heading").addEventListener("click", helloHeading)
+function helloHeading() {
+    alert("hello Heading");
+}
