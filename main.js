34b9896f6a99ffba0c73423c470a06f623e0eb5c
b.anwar <109045924+fatist13@users.noreply.github.com>
2022-10-04 14:00:03 -0600

    l

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+// ZE JAVASCRIPT
+
 document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  // ZE Input
+  let i1 = +document.getElementById("i1").value;
+  let i2 = +document.getElementById("i2").value;
+  let i3 = +document.getElementById("i3").value;
+  let i4 = +document.getElementById("i4").value;
+  let p1 = +document.getElementById("p1").value;
+
+  // ZE Process
+  let zegrade = (i1 + i2 + i3 + i4 + p1) / 5;
+  // Ze Output
+  document.getElementById("output").innerHTML = zegrade + "%";
+}
