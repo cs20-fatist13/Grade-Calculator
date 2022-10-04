fda4b5b36fa48e40f233e27813d4036196ca10bf
b.anwar <109045924+fatist13@users.noreply.github.com>
2022-10-04 13:44:11 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-// JavaScript Code
-// Button Event Listener
 document.getElementById("btn").addEventListener("click", btnClicked);
-// Input
-let name = document.getElementById("btn1").value.toLowerCase();
-let outputEl = document.getElementById("currentTile");
