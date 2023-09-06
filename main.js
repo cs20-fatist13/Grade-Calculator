ad83789b27550db75694fa4a6b31182437b58096
Basaam Anwar <109045924+fatist13@users.noreply.github.com>
2023-09-06 13:18:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
-// ZE JAVASCRIPT
+
 
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
-  // ZE Input
+  
   let i1 = +document.getElementById("i1").value;
   let i2 = +document.getElementById("i2").value;
   let i3 = +document.getElementById("i3").value;
   let i4 = +document.getElementById("i4").value;
   let p1 = +document.getElementById("p1").value;
 
-  // ZE Process
-  let zegrade = (i1 + i2 + i3 + i4 + p1) / 5;
-  // Ze Output
-  document.getElementById("output").innerHTML = zegrade + "%";
+
+  let grade = (i1 + i2 + i3 + i4 + p1) / 5;
+  
+  document.getElementById("output").innerHTML = grade + "%";
+}
+
+function getLetterGrade() {
+  let grade = +document.getElementById("output").innerHTML;
+  let letterGrade = "";
+
+  if (grade >= 90) {
+    letterGrade = "A";
+  } else if (grade >= 80) {
+    letterGrade = "B";
+  } else if (grade >= 70) {
+    letterGrade = "C";
+  } else if (grade >= 60) {
+    letterGrade = "D";
+  } else {
+    letterGrade = "F";
+  }
+
+  document.getElementById("letterGrade").innerHTML = letterGrade;
 }
